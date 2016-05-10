a1f73336e07eaed4f26de084f4a9df7d4da52652
Ataíde <ataide.neto31@gmail.com>
2016-05-10 14:53:42 -0300

    Classe periodo

---
+++ src/cc20152/sisufal/models/Periodo.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cc20152.sisufal.models;
+
+/**
+ *
+ * @author AtaideAl
+ */
+public class Periodo {
+    
+    private int id;
+    private String nome;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
