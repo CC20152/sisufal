92d9a2fee16abc83690111a3da28153553253350
Ataíde <ataide.neto31@gmail.com>
2016-05-23 15:55:44 -0300

    corrigindo erro

--- src/cc20152/sisufal/controllers/institucional/DiscenteController.java
+++ src/cc20152/sisufal/controllers/institucional/DiscenteController.java
@@ -1,8 +1,3 @@
-﻿/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cc20152.sisufal.controllers.institucional;
 
 import cc20152.sisufal.dao.impl.CursoDAO;
